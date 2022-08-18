eccd3f6d6c5434991bd4c5578c51d2e29f78f5d2
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-08-18 15:09:36 -0400

    checking if user data is null set user info to N/A

--- starter-code/script.js
+++ starter-code/script.js
@@ -13,18 +13,23 @@ const twitterLink = document.querySelector('.twitter-link');
 const company = document.querySelector('.company');
 
 const renderUserData = (data) => {
-  userImg.src = data.avatar_url;
-  actualName.innerText = data.name;
-  userName.innerText = '@' + data.login;
-  joinDate.innerText = data.created_at;
-  bio.innerText = data.bio;
+  userImg.src = data.avatar_url === null ? 'Not Available' : data.avatar_url;
+  actualName.innerText = data.name === null ? 'Not Available' : data.name;
+  userName.innerText = data.login === null ? 'Not Available' : '@' + data.login;
+  joinDate.innerText =
+    data.created_at === null ? 'Not Available' : data.created_at;
+  bio.innerText = data.bio === null ? 'Bio Not Available' : data.bio;
   reposAmount.innerText = data.public_repos;
-  followersAmount.innerText = data.followers;
-  followingAmount.innerText = data.following;
-  userLocation.innerText = data.location;
-  websiteLink.href = data.blog;
-  twitterLink.innerText = data.twitter_username;
-  company.innerText = data.company;
+  followersAmount.innerText =
+    data.followers === null ? 'Not Available' : data.followers;
+  followingAmount.innerText =
+    data.following === null ? 'Not Available' : data.following;
+  userLocation.innerText =
+    data.location === null ? 'Not Available' : data.location;
+  websiteLink.href = data.blog === null ? 'Not Available' : data.blog;
+  twitterLink.innerText =
+    data.twitter_username === null ? 'Not Available' : data.twitter_username;
+  company.innerText = data.company === null ? 'Not Available' : data.company;
 };
 
 fetch('https://api.github.com/users/jasminvillatoro')
