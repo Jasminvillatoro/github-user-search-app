31a8c25e9e71adb7c80058e298411f8267a59703
Jasminvillatoro <89315596+Jasminvillatoro@users.noreply.github.com>
2022-08-18 13:52:38 -0400

    created function to render the users information

--- starter-code/script.js
+++ starter-code/script.js
@@ -12,20 +12,24 @@ const websiteLink = document.querySelector('.website-link');
 const twitterLink = document.querySelector('.twitter-link');
 const company = document.querySelector('.company');
 
+const renderUserData = (data) => {
+  userImg.src = data.avatar_url;
+  actualName.innerText = data.name;
+  userName.innerText = '@' + data.login;
+  joinDate.innerText = data.created_at;
+  bio.innerText = data.bio;
+  reposAmount.innerText = data.public_repos;
+  followersAmount.innerText = data.followers;
+  followingAmount.innerText = data.following;
+  userLocation.innerText = data.location;
+  websiteLink.href = data.blog;
+  twitterLink.innerText = data.twitter_username;
+  company.innerText = data.company;
+};
+
 fetch('https://api.github.com/users/jasminvillatoro')
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
-    userImg.src = data.avatar_url;
-    actualName.innerText = data.name;
-    userName.innerText = '@' + data.login;
-    joinDate.innerText = data.created_at;
-    bio.innerText = data.bio;
-    reposAmount.innerText = data.public_repos;
-    followersAmount.innerText = data.followers;
-    followingAmount.innerText = data.following;
-    userLocation.innerText = data.location;
-    websiteLink.innerText = data.blog;
-    twitterLink.innerText = data.twitter_username;
-    company.innerText = data.company;
+    renderUserData(data);
   });
